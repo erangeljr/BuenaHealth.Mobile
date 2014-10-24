4d8c1b2d5006fd67aff4f0511c83089a9f2e4501
erangeljr <hacker@eddierangel.com>
2014-10-24 16:55:57 -0700

    Create File Access Helper Class
    For Android and iOS

---
+++ iOS/FileAccessHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BuenaHealth.Mobile.iOS
+{
+	public class FileAccessHelper
+	{
+		public FileAccessHelper()
+		{
+		}
+
+		public static string GetLocalFilePath(string filename)
+		{
+			string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			string libFolder = System.IO.Path.Combine(docFolder, "..", "Library","Databases");
+
+			if(!System.IO.Directory.Exists(libFolder))
+			{
+				System.IO.Directory.CreateDirectory(libFolder);
+			}
+
+			return System.IO.Path.Combine(libFolder,filename);
+		}
+
+
+	}
+}
+
