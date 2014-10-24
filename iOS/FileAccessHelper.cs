600df76df8dd5266f53c504dc98fbad2f229f932
erangeljr <hacker@eddierangel.com>
2014-10-24 16:56:35 -0700

    Comments

--- iOS/FileAccessHelper.cs
+++ iOS/FileAccessHelper.cs
@@ -2,12 +2,23 @@
 
 namespace BuenaHealth.Mobile.iOS
 {
+	/// <summary>
+	/// File access helper.
+	/// </summary>
 	public class FileAccessHelper
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BuenaHealth.Mobile.iOS.FileAccessHelper"/> class.
+		/// </summary>
 		public FileAccessHelper()
 		{
 		}
 
+		/// <summary>
+		/// Gets the local file path.
+		/// </summary>
+		/// <returns>The local file path.</returns>
+		/// <param name="filename">Filename.</param>
 		public static string GetLocalFilePath(string filename)
 		{
 			string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
