7031019402db905e9017dd0749f8c0cbba41c0d4
Eddie Rangel <hacker@eddierangel.com>
2015-01-22 22:08:49 -0800

    Create VitalSigns Model

---
+++ BuenaHealth.Mobile/BuenaHealth.Mobile/Models/VitalSigns.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuenaHealth.Mobile.Models
+{
+    public class VitalSigns
+    {
+        private float Height { get; set; }
+        public float Weight { get; set; }
+        public int Systolic { get; set; }
+        public int Diastolic { get; set; }
+    }
+}
