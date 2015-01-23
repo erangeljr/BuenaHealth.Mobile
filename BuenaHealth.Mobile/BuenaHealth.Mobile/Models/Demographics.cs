e7f833a953ac48579cefb9773088ce80a349fead
Eddie Rangel <hacker@eddierangel.com>
2015-01-22 22:21:16 -0800

    Add Demographics Model

---
+++ BuenaHealth.Mobile/BuenaHealth.Mobile/Models/Demographics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuenaHealth.Mobile.Models
+{
+    public class Demographics
+    {
+        public string Language { get; set; }
+        public string Sex { get; set; }
+        public string Race { get; set; }
+        public string Ethnicity { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
