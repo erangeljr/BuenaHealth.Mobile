a2fc1dbfbdbf6337bbbac2b2ada695fcbe3e6121
Eddie Rangel <hacker@eddierangel.com>
2015-01-22 22:11:18 -0800

    Add Notes Model

---
+++ BuenaHealth.Mobile/BuenaHealth.Mobile/Models/Notes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuenaHealth.Mobile.Models
+{
+    public class Notes
+    {
+        public string Summmary { get; set; }
+    }
+}
