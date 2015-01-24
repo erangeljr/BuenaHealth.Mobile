421d9152a351baced9b90b0ceb546e99b9e6d83f
Eddie Rangel <hacker@eddierangel.com>
2015-01-24 14:48:13 -0800

    Add FileAccessHelper to Project specific projects

---
+++ BuenaHealth.Mobile/BuenaHealth.Mobile.WinPhone/FileAccessHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuenaHealth.Mobile.WinPhone
+{
+    public class FileAccessHelper
+    {
+        public static string GetLocalFilePath()
+        {
+        }
+    }
+}
---
+++ BuenaHealth.Mobile/BuenaHealth.Mobile.iOS/FileAccessHelper .cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace BuenaHealth.Mobile.iOS
+{
+    public class FileAccessHelper
+    {
+        public static string GetLocalFilePath()
+        {
+        }
+    }
+}
