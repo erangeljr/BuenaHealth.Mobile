b17192110b408f64c6932742dcba61c601cedb13
Eddie Rangel <hacker@eddierangel.com>
2015-01-26 22:18:30 -0800

    Renamed Base Repository

---
+++ BuenaHealth.Mobile/BuenaHealth.Mobile/Infrastructure/BuenaHealthBaseRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BuenaHealth.Mobile.Interfaces;
+using BuenaHealth.Mobile.Models;
+using SQLite.Net;
+using SQLite.Net.Interop;
+
+namespace BuenaHealth.Mobile.Infrastructure
+{
+    public class BuenaHealthBaseRepository<T> : IRepository<T> where T : class
+    {
+         private SQLiteConnection dbConn;
+        
+        public string StatusMessage { get; set; }
+        public BuenaHealthRepository(ISQLitePlatform sqlitePlatform, string dbPath)
+        {
+            //initialize a new SQLiteConnection 
+            if (dbConn == null)
+            {
+                dbConn = new SQLiteConnection(sqlitePlatform, dbPath);
+
+                //create the Person table
+                dbConn.CreateTable<T>();
+            }
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Single(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T SingleOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T First(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attach(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
