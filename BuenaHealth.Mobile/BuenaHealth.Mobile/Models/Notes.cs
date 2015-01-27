ec4ddad43541067c20f6b649314a202a33ef491c
Eddie Rangel <hacker@eddierangel.com>
2015-01-26 21:05:26 -0800

    Adding SQLite attributes

--- BuenaHealth.Mobile/BuenaHealth.Mobile/Models/Notes.cs
+++ BuenaHealth.Mobile/BuenaHealth.Mobile/Models/Notes.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite.Net.Attributes;
 
 namespace BuenaHealth.Mobile.Models
 {
+    [Table("notes")]
     public class Notes
     {
+        [PrimaryKey, AutoIncrement]
         public int NoteId { get; set; }
         public string Summmary { get; set; }
     }
