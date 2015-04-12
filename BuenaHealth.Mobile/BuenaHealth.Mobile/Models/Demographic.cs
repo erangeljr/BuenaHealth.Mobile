561b17c0750686f03d6477c9e224545f0fe11501
Eddie Rangel <hacker@eddierangel.com>
2015-04-11 20:25:13 -0700

    Adding Models

---
+++ BuenaHealth.Mobile/BuenaHealth.Mobile/Models/Demographic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuenaHealth.Mobile.Models
+{
+    public class Demographic
+    {
+        public int DemographicId { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Language { get; set; }
+        public string Sex { get; set; }
+        public string Race { get; set; }
+        public string Ethnicity { get; set; }
+    }
+}
---
+++ BuenaHealth.Mobile/BuenaHealth.Mobile/Models/VitalSigns.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuenaHealth.Mobile.Models
+{
+    public class VitalSign
+    {
+        public int VitalSignId { get; set; }
+        public float Height { get; set; }
+        public float Weight { get; set; }
+        public int Systolic { get; set; }
+        public int Diastolic { get; set; }
+    }
+}
