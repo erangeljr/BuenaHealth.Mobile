ae36afa8b9a1834b5b44c10d652f136be55e939f
Eddie Rangel <hacker@eddierangel.com>
2015-04-17 17:01:32 -0700

    Updating Titles

---
+++ BuenaHealth.Mobile/BuenaHealth.Mobile/Menu/MenuListData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuenaHealth.Mobile.Menu
+{
+    public class MenuListData : List<MenuItem>
+    {
+        public MenuListData()
+        {
+
+            this.Add(new MenuItem()
+            {
+                Title = "User Profile",
+                IconSource = "profile.png",
+                TargetType = typeof(UserPage)
+            });
+
+            this.Add(new MenuItem()
+            {
+                Title = "Demographics",
+                IconSource = "demographics.png",
+                TargetType = typeof(DemographicPage)
+            });
+
+            this.Add(new MenuItem()
+            {
+                Title = "Vital Signs",
+                IconSource = "vitalsigns.png",
+                TargetType = typeof(VitalSignPage)
+            });
+
+            this.Add(new MenuItem()
+            {
+                Title = "Opportunities",
+                IconSource = "opportunities.png",
+                TargetType = typeof(MainPage)
+            });
+        }
+    }
+}
---
+++ BuenaHealth.Mobile/BuenaHealth.Mobile/Menu/RootPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace BuenaHealth.Mobile.Menu
+{
+    public class RootPage : MasterDetailPage
+    {
+        public RootPage()
+        {
+            var menuPage = new MenuPage();
+
+            menuPage.Menu.ItemSelected += (sender, e) => NavigateTo(e.SelectedItem as MenuItem);
+
+            Master = menuPage;
+            Detail = new NavigationPage(new MainPage());
+        }
+
+        void NavigateTo(MenuItem menu)
+        {
+            Page displayPage = (Page)Activator.CreateInstance(menu.TargetType);
+
+            Detail = new NavigationPage(displayPage);
+
+            IsPresented = false;
+        }
+    }
+}
